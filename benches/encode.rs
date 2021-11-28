60113b56b18a2fd81c87b2c03d43972fef7968be
TheImaginaryOne <johnathanleung@hotmail.co.nz>
2021-11-28 17:48:21 +1300

    Write encoder benchmarks

---
+++ benches/encode.rs
@@ -0,0 +1,30 @@
+use base64::basic::BasicEncoder;
+use base64::traits::Base64Encoder;
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId, Bencher};
+use rand::{rngs::SmallRng, RngCore, SeedableRng};
+
+fn random_bytes(len: usize) -> Vec<u8> {
+    let mut vec = vec![0; len];
+    let mut rng = SmallRng::from_seed(*b"behejw##&*sbry219lkmzlk;-=[[3;nd");
+    rng.fill_bytes(&mut vec[..]);
+    vec
+}
+
+// There are benchmarks for basic encoder and (TODO) fast encoder.
+
+pub fn basic_encoder(b: &mut Bencher, &size: &usize) {
+    let input = random_bytes(size);
+    let mut output = vec![0; 4 * (1 + size / 3)];
+    let encoder = BasicEncoder::new();
+    b.iter(|| encoder.encode(&input, black_box(&mut output)));
+}
+
+fn bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Encoder");
+    for i in [1024, 1024 * 256, 1024 * 1024 * 4] {
+        group.bench_with_input(BenchmarkId::new("Basic", i), &i, basic_encoder);
+    }
+}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
